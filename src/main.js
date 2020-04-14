65cd7a88a5417bfeb038dfd4e49a76de6280f877
Ryan Duff <ryanduff613@gmail.com>
2020-04-14 16:32:48 -0700

    add dependencies for and import to main.js for jquery, bootstrap and styles.css

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
-/*
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import { Triangle } from "./triangle";
+
 $(document).ready(function() {
   $("form#triangle").submit(function(event) {
     event.preventDefault();
     var s1 = parseInt($("input#s1").val());
     var s2 = parseInt($("input#s2").val());
     var s3 = parseInt($("input#s3").val());
+    var userTriangle = new Triangle(s1,s2,s3);
+    $("#output").text(result):
   });
 });  
-*/
